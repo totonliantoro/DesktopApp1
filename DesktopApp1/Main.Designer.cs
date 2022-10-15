16517c662955b9b102ec41700a88f013f3f6330c
Toton Liantoro <toton.liantoro@gmail.com>
2022-10-15 14:07:38 +1100

    Add files via upload

---
+++ DesktopApp1/Main.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace MyAssessment
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lstFile = new System.Windows.Forms.ListBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.btnNextRecord = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblTheResult = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnOpen.Location = new System.Drawing.Point(126, 298);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(98, 34);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lstFile
+            // 
+            this.lstFile.FormattingEnabled = true;
+            this.lstFile.ItemHeight = 16;
+            this.lstFile.Location = new System.Drawing.Point(260, 188);
+            this.lstFile.Name = "lstFile";
+            this.lstFile.Size = new System.Drawing.Size(130, 276);
+            this.lstFile.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.SystemColors.Info;
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStatus.Location = new System.Drawing.Point(260, 126);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(130, 37);
+            this.lblStatus.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnExit.Location = new System.Drawing.Point(504, 429);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(130, 35);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "openFileDialog1";
+            // 
+            // btnNextRecord
+            // 
+            this.btnNextRecord.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnNextRecord.Location = new System.Drawing.Point(126, 358);
+            this.btnNextRecord.Name = "btnNextRecord";
+            this.btnNextRecord.Size = new System.Drawing.Size(98, 34);
+            this.btnNextRecord.TabIndex = 4;
+            this.btnNextRecord.Text = "Next Record";
+            this.btnNextRecord.UseVisualStyleBackColor = false;
+            this.btnNextRecord.Click += new System.EventHandler(this.btnNextRecord_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnSort.Location = new System.Drawing.Point(126, 419);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(98, 30);
+            this.btnSort.TabIndex = 5;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = false;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.Location = new System.Drawing.Point(495, 188);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(81, 20);
+            this.lblSearch.TabIndex = 6;
+            this.lblSearch.Text = "Search for";
+            // 
+            // txtFind
+            // 
+            this.txtFind.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFind.Location = new System.Drawing.Point(571, 184);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(123, 22);
+            this.txtFind.TabIndex = 7;
+            this.txtFind.TextChanged += new System.EventHandler(this.txtFind_TextChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(501, 264);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(61, 21);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "Result";
+            // 
+            // lblTheResult
+            // 
+            this.lblTheResult.BackColor = System.Drawing.SystemColors.Info;
+            this.lblTheResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTheResult.Location = new System.Drawing.Point(571, 255);
+            this.lblTheResult.Name = "lblTheResult";
+            this.lblTheResult.Size = new System.Drawing.Size(168, 30);
+            this.lblTheResult.TabIndex = 9;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSearch.Location = new System.Drawing.Point(786, 184);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(83, 24);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 565);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblTheResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnNextRecord);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lstFile);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assessment1_Toton";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ListBox lstFile;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.Button btnNextRecord;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblTheResult;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ DesktopApp1/Main.cs
@@ -0,0 +1,142 @@
+﻿//***********************************************************************************************
+//Programmer: Toton Liantoro
+//Date: 8 Sepetember 2022
+//Software: Microsoft Visual Studio 2022 Community Edition
+//Platform: Microsoft Windows 11 64-bit
+//Purpose: To complete the assignment 1 Subject: Apply Advance Object-Oriented Language Skills
+//***********************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyAssessment
+{
+    public partial class FrmMain : Form
+    {
+        private StreamReader fileReader;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+       
+        string fileName;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //Set filter for open File dialog 
+            dlgOpen.Filter = "Text Files | *.txt";
+            if (dlgOpen.ShowDialog() != DialogResult.Cancel)
+                fileName = dlgOpen.FileName;
+
+            //Open file for read access
+            FileStream input = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+            fileReader = new StreamReader(input);
+            btnOpen.Enabled = false;
+        }
+
+        private void btnNextRecord_Click(object sender, EventArgs e)
+        {
+            lstFile.Items.Clear();
+            string myNumbers;
+            StreamReader file = new StreamReader(fileName);
+            List<int> list = new List<int>();
+            while ((myNumbers = file.ReadLine()) != null)
+            {
+                list.Add(int.Parse(myNumbers));
+            }
+
+            int[] arr = list.ToArray();
+            foreach (int item in arr)
+            {
+                lstFile.Items.Add(item);
+            }
+            lblStatus.Text = "Unsorted Data";
+            lblStatus.BackColor = System.Drawing.Color.Cyan;
+            lstFile.BackColor = System.Drawing.Color.Orange;
+        }
+
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            lstFile.Items.Clear();
+            string myNumbers;
+            StreamReader file = new StreamReader(fileName);
+            List<int> list = new List<int>();
+            while ((myNumbers = file.ReadLine()) != null)
+            {
+                list.Add(int.Parse(myNumbers));
+            }
+
+            int[] arr = list.ToArray();
+            Array.Sort(arr);
+            foreach (int item in arr)
+            {
+                lstFile.Items.Add(item);
+            }
+            lblStatus.Text = "Sorted Data";
+            lblStatus.BackColor = System.Drawing.Color.Cornsilk;
+            // Change backcolor of listbox to green (sorted)
+            lstFile.BackColor = System.Drawing.Color.LightGreen;
+
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int [] data = lstFile.Items.OfType<int>().ToArray();
+            int top = 0;
+            int mid = 99;
+            int bottom = lstFile.Items.Count - 1;
+            int key;
+            bool result = false;
+            key = Convert.ToInt32(txtFind.Text);
+            while (bottom >= top)
+            {
+                mid = (top + bottom) / 2;
+                if (key == data[mid])
+                {
+                    result = true;
+                    break;
+                }
+                else
+                    if (key < data[mid])
+                        bottom = mid - 1;
+                    else 
+                        top = mid + 1;
+            }
+
+            if (result)
+                lblTheResult.Text = "Item found as index " + Convert.ToString(mid);
+
+            else
+                lblTheResult.Text = "Item not found";
+
+        }
+
+        private void txtFind_TextChanged(object sender, EventArgs e)
+        {
+            btnSearch.Enabled = true;
+            btnSearch.BackColor = System.Drawing.Color.Cyan;
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            fileReader.Close();
+            Application.Exit();
+        }
+
+      
+    }
+}
